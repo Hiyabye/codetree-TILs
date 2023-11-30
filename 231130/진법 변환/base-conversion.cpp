6bd3dc050d5191a39cd000d4d33e641508138a06
Jihoon Lee <hiyabye0465@gmail.com>
2023-11-30 16:29:33 +0900

    Title: 진법 변환, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231130/진법 변환/base-conversion.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int power(int base, int exp) {
+    if (exp == 0) return 1;
+    if (exp == 1) return base;
+    int half = power(base, exp/2);
+    if (exp & 1) return half * half * base;
+    else return half * half;
+}
+
+int main() {
+    string n; int m;
+    cin >> n >> m;
+    reverse(n.begin(), n.end());
+
+    int ans = 0;
+    for (int i=0; i<n.length(); i++) {
+        ans += (n[i] - '0') * power(m, i);
+    }
+    
+    cout << ans;
+    return 0;
+}
