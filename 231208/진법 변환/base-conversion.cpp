01c0c23b0931c71b3d24f5351c7aff53f406fe66
Jihoon Lee <hiyabye0465@gmail.com>
2023-12-08 21:47:44 +0900

    Title: 진법 변환, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231208/진법 변환/base-conversion.cpp
+++ 231208/진법 변환/base-conversion.cpp
@@ -5,12 +5,11 @@ using namespace std;
 int main() {
     string n; int m; cin >> n >> m;
 
-    int ans = 0;
-    for (int i=n.length()-1, base=1; i>=0; i--) {
-        ans += base * (n[i]-'0');
+    long long ans = 0, base = 1;
+    for (int i=n.length()-1; i>=0; i--) {
+        ans += base * (n[i] - '0');
         base *= m;
     }
-
     cout << ans;
     return 0;
 }
