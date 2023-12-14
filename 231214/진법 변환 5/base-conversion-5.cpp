206c5376d0ab3c58b71f7a1d01a334638a7cc5ca
Jihoon Lee <hiyabye0465@gmail.com>
2023-12-14 14:28:22 +0900

    Title: 진법 변환 5, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231214/진법 변환 5/base-conversion-5.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int a, b; cin >> a >> b;
+    string s = "";
+
+    while (a) {
+        s += (a & 1) + '0';
+        a >>= 1;
+    }
+
+    string ans = "";
+    if (b == 2) {
+        ans = s;
+    } else if (b == 8) {
+        while (s.length() % 3) s += '0';
+        for (int i=0; i<s.length(); i+=3) {
+            ans += ((s[i]-'0')+(s[i+1]-'0')*2+(s[i+2]-'0')*4)+'0';
+        }
+    } else {
+        while (s.length() % 4) s += '0';
+        for (int i=0; i<s.length(); i+=4) {
+            int val = (s[i]-'0') + (s[i+1]-'0')*2 + (s[i+2]-'0')*4 + (s[i+3]-'0')*8;
+            ans += (val > 9 ? (char)(val-10+'a') : (char)(val+'0'))
+        }
+    }
+    reverse(ans.begin(), ans.end());
+    cout << ans;
+    return 0;
+}
