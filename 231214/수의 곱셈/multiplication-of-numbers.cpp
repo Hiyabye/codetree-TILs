b83fa68385d8b91d71f43dbafccb6815b0568650
Jihoon Lee <hiyabye0465@gmail.com>
2023-12-14 13:03:04 +0900

    Title: 수의 곱셈, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 231214/수의 곱셈/multiplication-of-numbers.cpp
+++ 231214/수의 곱셈/multiplication-of-numbers.cpp
@@ -4,6 +4,14 @@ using namespace std;
 int main() {
     int a, b, c; cin >> a >> b >> c;
 
-    cout << a * b * c;
+    int ans = 1;
+    if (a & 1) ans *= a;
+    if (b & 1) ans *= b;
+    if (c & 1) ans *= c;
+    if (ans == 1 && a != 1 && b != 1 && c != 1) ans *= a * b * c;
+
+    cout << ans;
     return 0;
 }
+
+// 2 4 1
