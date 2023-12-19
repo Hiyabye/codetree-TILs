047faee659ca235ee8efef7517eddce25eecc537
Jihoon Lee <hiyabye0465@gmail.com>
2023-12-19 14:41:39 +0900

    Title: 소수와 합성수, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 231219/소수와 합성수/prime-composition.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+
+    // one
+    if (n == 1) { cout << "one"; return 0; }
+
+    // sieve
+    vector<bool> prime(70000, true);
+    prime[0] = prime[1] = false;
+    for (int i=4; i<70000; i+=2) prime[i] = false;
+    for (int i=3; i*i<70000; i+=2) {
+        if (!prime[i]) continue;
+        for (int j=i*i; j<70000; j+=2) prime[j] = false;
+    }
+
+    // prime & composition
+    if (n < 70000 && prime[n]) { cout << "prime"; return 0; }
+    if (!(n & 1)) { cout << "composition"; return 0; }
+    for (int i=3; i*i<=n; i+=2) {
+        if (!prime[i]) continue;
+        if (!(n % i)) { cout << "composition"; return 0; }
+    }
+    cout << "prime";
+    return 0;
+}
